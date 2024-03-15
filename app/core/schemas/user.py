972a8bb360e3981ff82aa7de1bfb912b402d2bed
Joseph Silva <119358195+JosephSC0121@users.noreply.github.com>
2024-03-14 23:40:04 -0500

    Auth working, need a huge refactor

---
+++ app/core/schemas/user.py
@@ -0,0 +1,13 @@
+
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    username: str
+    email: str or None = None
+    full_name: str or None = None
+    disabled: bool or None = None
+
+
+class UserInDB(User):
+    hashed_password: str
--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import  FastAPI
+from fastapi import FastAPI
+from v1.endpoints.auth import router as auth_router
+from v1.endpoints.user import router as user_router
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return { "message" : "hola!"}
+app.include_router(auth_router)
+app.include_router(user_router)
